5f8effa8e2c42431a774bedff15cb411f0053a08
dontstealmyusernameahhg <qwertyuiopreeyansh@gmail.com>
2025-08-06 19:06:20 +1200

    student

---
+++ orint.py
@@ -0,0 +1,4 @@
+class student:
+    grade = -100000000000000000000000000000000000
+print("gogogogogo gagagaga gogogo gagagagaga gogogogogo gagagagag (it means:hello im a student of grade )", student.grade)
+object = student()
